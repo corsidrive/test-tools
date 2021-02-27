fa29eed551374a7a7b41b299a1a50b55a332983a
ryoga <sarassoroberto@gmail.com>
2021-02-27 15:41:37 +0100

    aggiunta docs con

--- testTools.php
+++ testTools.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * @file
+ * Funzioni utili per agevolare i test
+ * php version 7.0
+ * @author Sarasso Roberto <sarassoroberto@gmail.com>
+ */
 
+
+/**
+ * Controlla se un affermazione è corretta
+ * 
+ * @var    $expected valore atteso
+ * @var    $actual valore da controllare
+ * @var    $description eventuale descrizione del test
+ * @var    $line
+ * @return void
+ */
 function assertEquals($expected,$actual,$description = '',$line = NULL){
+    
     if($expected !== $actual){
         echo "FAIL: $description \n";
         echo "atteso  '$expected' (".gettype($expected).")\n";
